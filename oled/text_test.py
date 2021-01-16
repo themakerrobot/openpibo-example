672361bf78ce6a80115bd68ec33481536db630de
leeyunjai82 <leeyunjai@circul.us>
2021-01-16 16:52:04 +0900

    update

--- oled/text_test.py
+++ oled/text_test.py
@@ -13,7 +13,8 @@ def oled_f():
   oObj = cOled(conf=cfg)
   oObj.set_font(size=15)
   
-  oObj.draw_text((10,10), "안녕? 난 파이보야")
+  oObj.draw_text((0, 0), "안녕? 난 파이보야 ")
+  oObj.draw_text((0,20), "☆  ★ ")
   oObj.show()
 
 '''
